d490130c5f578764b6478b3f2e3bff3be51a219e
yocosaka <sakayoco55@gmail.com>
2023-10-23 16:39:30 +0900

    Add Link to review username

--- components/movies/MovieDetail/Reviews/Review/Review.tsx
+++ components/movies/MovieDetail/Reviews/Review/Review.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import clsx from 'clsx';
 import { ReviewState } from '@/types/movies';
 import { excerptText } from '@/utils';
@@ -14,26 +15,40 @@ interface ReviewPropsType {
 
 export const Review = ({ review, updateDate }: ReviewPropsType) => {
   const [showFull, setShowFull] = useState<boolean>(false);
+  const imagePath = review.imageUrl
+    ? review.imageUrl
+    : review.author_details.avatar_path
+    ? `${process.env.NEXT_PUBLIC_TMDB_AUTHOR_IMAGE_URL}${review.author_details.avatar_path}`
+    : undefined;
+  const link = {
+    href: review.userId ? `/profile/${review.userId}` : review.url,
+    target: review.userId ? undefined : '_blank',
+    rel: review.userId ? undefined : 'noopener',
+  };
 
   return (
     <div className={styles.review}>
       <div className={styles.reviewHeader}>
-        <div className={styles.author}>
-          {review.author_details.avatar_path ? (
-            <div className={styles.authorImageWrapper}>
-              <Image
-                src={`${process.env.NEXT_PUBLIC_TMDB_AUTHOR_IMAGE_URL}${review.author_details.avatar_path}`}
-                alt={review.author}
-                fill
-                style={{ objectFit: 'cover' }}
-                sizes="(max-width: 768px) 50vw, (max-width: 1200px) 20vw"
-              />
-            </div>
-          ) : (
-            <BiSolidUserCircle className={styles.authorIcon} />
-          )}
-          <div className={styles.authorName}>{review.author}</div>
-        </div>
+        <Link href={link.href} target={link.target} rel={link.rel}>
+          <div className={styles.author}>
+            {imagePath ? (
+              <div className={styles.authorImageWrapper}>
+                <Image
+                  src={imagePath}
+                  alt={review.author}
+                  fill
+                  style={{ objectFit: 'cover' }}
+                  sizes="(max-width: 768px) 50vw, (max-width: 1200px) 20vw"
+                />
+              </div>
+            ) : (
+              <>
+                <BiSolidUserCircle className={styles.authorIcon} />
+              </>
+            )}
+            <div className={styles.authorName}>{review.author}</div>
+          </div>
+        </Link>
         <div
           className={styles.date}
         >{`${updateDate.getFullYear()}/${updateDate.getMonth()}`}</div>
