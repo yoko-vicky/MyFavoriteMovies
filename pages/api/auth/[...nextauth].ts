4f339276956b0c560f8c70e925703442378003c5
yocosaka <sakayoco55@gmail.com>
2023-11-08 20:01:33 +0900

    Update name of nextAuth api

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,6 +4,6 @@ import { authOptions } from '@/lib/nextAuth';
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
-export default async function auth(req: any, res: any) {
+export default async function handler(req: any, res: any) {
   return await NextAuth(req, res, authOptions as AuthOptions);
 }
